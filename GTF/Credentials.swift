5861b2c6e8f4466b6839b05e458a909c1e5a52c7
Amber Spadafora <ahelenespadafora@gmail.com>
2017-07-23 17:14:36 -0700

    aeris sdk project

---
+++ GTF/Credentials.swift
@@ -0,0 +1,17 @@
+//
+//  credentials.swift
+//  GTF
+//
+//  Created by Amber Spadafora on 7/23/17.
+//  Copyright © 2017 Amber Spadafora. All rights reserved.
+//
+
+import Foundation
+
+class Credentials {
+    
+    var shared = Credentials()
+    
+    var apiKey = "enter your api key here"
+    var apiSecret = "Enter your api secret code here"
+}
